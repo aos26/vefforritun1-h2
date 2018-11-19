d71c9dddbb2c8750a364c52316bf6e99c5d8c886
Erling Oskar Kristjansson <eokristjans@gmail.com>
2018-11-19 10:43:13 +0000

    glósaði í .js files

--- src/lib/list.js
+++ src/lib/list.js
@@ -10,3 +10,35 @@ export default class List {
     empty(this.container);
   }
 }
+
+
+
+/**
+ * Veit ekkert hvar á að setja neitt eða hvar á að byrja
+ * Hér eru allavega breytur sem við munum þurfa, mögulega á röngu format
+ */
+
+const DATA_URL = '../lectures.json'
+
+const [{
+  slug,
+  title,
+  category,
+  image,
+  thumbnail,
+  content
+}] = lecture;
+
+
+// Þurfum að lesa efnið úr lectures á fyrirlestrasíðunni og birta efni á viðeigandi hátt
+const [{
+  youtube, // inniheldur hlekk á youtube myndband
+  text, // inniheldur gögn þar sem \n merkir á milli málsgreina, þ.e.a.s. texta skal birta innan <p>, skipt á \n
+  quote, //  inniheldur tilvitnun, aukalega getur verið `attribute` með þeim sem vitnað er í
+  image, // inniheldur slóð á mynd, aukalega getur verið ` caption`  með texta með mynd
+  heading, // inniheldur fyrirsögn
+  list, // inniheldur fylki af textum í lista
+  code // inniheldur kóða þar sem bil og nýjar línur skipta máli
+}] = datatype; // content
+
+const 
