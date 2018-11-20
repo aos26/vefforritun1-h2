516a1c61a9e6b21b626cfea497be3ed4eeebcb88
Erling Oskar Kristjansson <eokristjans@gmail.com>
2018-11-20 01:13:50 +0000

    Fallið load() í list.js hleður inn efninu úr lectures.json;  Föllin displayAllLecturesOnIndex(...) og displayLectureOnIndex(...) birta allt það efni á index.htmlgit add *

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,99 @@
-export function empty(element) { /* eslint-disable-line */
+/**
+ * Tæmir HTML element
+ */
+export function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
+}
+
+/**
+ * Fyrir: Element el, almennt sjálfsagt .list
+ *        lectInfo er fylki með útvöldum strengjum úr lectures.json
+ *          þeir innihalda title, category og thumbnail
+ * Eftir: Smíðar eftirfarandi HTML element og setur það inní <div class="grid__row list">
+         <div class="grid__col"><!--Sample content, þetta á raunverulega að vera lesið úr lectures.json-->
+          <div class="lecture">
+            <div class="lecture__title"><h2 class="headline2">lectureTitleString</h2></div>
+            <div class="lecture__category"><h3 class="headline3">lectureCategoryString</h3></div>
+            <div class="lecture__image"><img src="lectureThumbString" alt="" class="img"></div>
+          </div>
+        </div>
+ */
+function displayLectureOnIndex(el, lectInfo) {
+  const element = el;
+  const lectureTitleString = lectInfo[0];
+  const lectureCategoryString = lectInfo[1];
+  let lectureThumbString = lectInfo[2];
+  if (lectureThumbString === undefined) lectureThumbString = '';
+
+  // Númer 1
+  const gridCol = document.createElement('div');
+  gridCol.className = 'grid__col';
+  // Númer 2
+  const lecture = document.createElement('div');
+  lecture.className = 'lecture';
+  // 3 hlutir inní Númer 2
+  const lectureTitle = document.createElement('div');
+  lectureTitle.className = 'lecture__title';
+  const lectureCategory = document.createElement('div');
+  lectureCategory.className = 'lecture__category';
+  const lectureImage = document.createElement('div');
+  lectureImage.className = 'lecture__image';
+  // 3 hlutir, einn inní hvern ofangreindra hluta
+  const lectureTitleH2 = document.createElement('h2');
+  lectureTitleH2.className = 'headline2';
+  lectureTitleH2.appendChild(document.createTextNode(lectureTitleString));
+  const lectureCategoryH3 = document.createElement('h3');
+  lectureCategoryH3.className = 'headline3';
+  lectureCategoryH3.appendChild(document.createTextNode(lectureCategoryString));
+  const lectureImageImg = document.createElement('img');
+  lectureImageImg.className = 'img';
+  lectureImageImg.src = lectureThumbString;
+
+  // Púslum þessu saman
+  lectureTitle.appendChild(lectureTitleH2);
+  lectureCategory.appendChild(lectureCategoryH3);
+  lectureImage.appendChild(lectureImageImg);
+  lecture.appendChild(lectureTitle);
+  lecture.appendChild(lectureCategory);
+  lecture.appendChild(lectureImage);
+  gridCol.appendChild(lecture);
+  element.appendChild(gridCol);
+}
+
+
+export function displayAllLecturesOnIndex(el, lectKeys, allLects) {
+  const element = el;
+  const lectureKeys = lectKeys;
+  const allLectures = allLects;
+  console.log(typeof allLectures); // eslint-disable-line no-console
+  console.log(allLectures.length); // eslint-disable-line no-console
+
+  allLectures.forEach((k) => {
+    const lectureInfo = [k[lectureKeys[0]], k[lectureKeys[1]], k[lectureKeys[2]]];
+    displayLectureOnIndex(element, lectureInfo);
+  });
+
+
+  /*
+  allLectures.forEach((k) => {
+    console.log(typeof k); // eslint-disable-line no-console
+    const kStr = JSON.stringify(k);
+    console.log(kStr); // eslint-disable-line no-console
+    const kP = JSON.parse(kStr);
+    console.log(kP); // eslint-disable-line no-console
+  });
+  console.log(typeof allLectures); // eslint-disable-line no-console
+  */
+
+  /*
+  allLectures.forEach((i) => {
+    const lecture = allLectures[i];
+    lectureKeys.forEach((k) => {
+      const lectureInfo = lecture[k];
+      displayLectureOnIndex(element, lectureInfo);
+    });
+  });
+  */
 }
--- src/lib/list.js
+++ src/lib/list.js
@@ -1,24 +1,55 @@
-import { empty } from './helpers';
+import { empty, displayAllLecturesOnIndex } from './helpers';
 
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    /**
+     * FILTER BUTTONS
+     * Þurfum örugglega að finna HTML, CSS og JS takkana:
+     this.htmlButt = document.querySelector('html-butt');
+     this.cssButt = document.querySelector('css-butt');
+     this.jsButt = document.querySelector('js-butt');
+     */
   }
 
+  /**
+   * Hleður inn efninu úr lectures.json
+   */
   load() {
     // Tæmir efnið úr þessum container.
     empty(this.container);
+
+    // Þetta er fylkið með lyklum að efni á forsíðuna, þurfum svo annað fyrir fyrirlestur.html
+    const lectureKeys0 = ['title', 'category', 'thumbnail'];
+
+    const DATA_URL = '/lectures.json';
+    /**
+     * Sækir gögnin úr lectures.json
+     */
+    fetch(DATA_URL)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Villa við að sækja gögn');
+      })
+      .then((data) => {
+        displayAllLecturesOnIndex(this.container, lectureKeys0, data.lectures);
+      })
+      .catch((error) => {
+        console.error(error); // eslint-disable-line
+      });
+    /**
+     * FILTER BUTTONS
+     * Þurfum örugglega event listeners fyrir takkana þrjá
+     this.htmlButt.addEventListener('click', this.click.bind(this));
+     this.cssButt.addEventListener('click', this.click.bind(this));
+     this.jsButt.addEventListener('click', this.click.bind(this));
+     */
   }
 }
 
-
-
-/**
- * Veit ekkert hvar á að setja neitt eða hvar á að byrja
- * Hér eru allavega breytur sem við munum þurfa, mögulega á röngu format
- */
-
-const DATA_URL = '../lectures.json'
+/*
 
 const [{
   slug,
@@ -41,4 +72,4 @@ const [{
   code // inniheldur kóða þar sem bil og nýjar línur skipta máli
 }] = datatype; // content
 
-const 
+*/
